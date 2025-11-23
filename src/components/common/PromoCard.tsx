42579a67b5c17cd957ea3d6d017724f2b66e7af1
RafiKK98 <rafikk1998@gmail.com>
2025-11-23 15:37:41 +0600

    promo card colors updated

--- src/components/common/PromoCard.tsx
+++ src/components/common/PromoCard.tsx
@@ -63,11 +63,22 @@ const PromoCard = ({
     >
       <Stack direction="column" gap={2} alignItems="center">
         <Stack direction="column" alignItems="center" gap={0.5}>
-          <Typography variant="subtitle1" fontWeight={700}>
+          <Typography
+            variant="subtitle1"
+            fontWeight={700}
+            sx={{
+              color: 'neutral.lighter',
+            }}
+          >
             {title}
           </Typography>
           <Stack gap={1} alignItems="center">
-            <Typography variant="caption" fontWeight={500} lineHeight={1.3}>
+            <Typography
+              variant="caption"
+              fontWeight={500}
+              lineHeight={1.3}
+              sx={{ color: 'neutral.lighter' }}
+            >
               {subTitle}
             </Typography>
             <Typography component="strong" variant="subtitle2" fontWeight={700} color="success">
@@ -85,10 +96,15 @@ const PromoCard = ({
             {proFeatures.map((feature, index) => (
               <Stack key={index} gap={0.4} alignItems="center">
                 <IconifyIcon
-                  icon="material-symbols:check"
+                  icon="material-symbols:check-circle-outline-rounded"
                   sx={{ fontSize: 16, color: 'success.main' }}
                 />
-                <Typography variant="caption" fontWeight={600}>
+                <Typography
+                  variant="caption"
+                  fontWeight={600}
+                  lineHeight={1.3}
+                  sx={{ color: 'neutral.lighter' }}
+                >
                   {feature}
                 </Typography>
               </Stack>
@@ -96,7 +112,7 @@ const PromoCard = ({
           </Stack>
         )}
 
-        <Button href={storeLink} target="_blank" fullWidth variant="contained" color="neutral">
+        <Button href={storeLink} target="_blank" fullWidth variant="soft" color="neutral">
           Explore now
         </Button>
       </Stack>
